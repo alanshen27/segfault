48d0ad7cecefe8e98ef3419969abee4b5c5e561c
Alan Shen <alan.shen27@gmail.com>
2026-05-21 23:02:21 +0200

    fix: prisma client running out of sessions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaPg(process.env.DATABASE_URL!);
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL!,
+    max: 1, // or 2/3, but start with 1
+  });
+    
+const adapter = new PrismaPg(pool);
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
